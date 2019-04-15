72b4312decc53a7a75545983711a707be583b9db
Stefan <moser.stefan1809@gmail.com>
2019-04-15 12:20:42 +0200

    main.py ======= Laid foundation for Hyperparamgirdsearch
    train.py
    ========
    Laid foundation for Hyperparamgirdsearch

--- Code/main.py
+++ Code/main.py
@@ -5,10 +5,18 @@ from scipy.io import arff
 
 from train import trainClassifiers
 
+def getAccuarcyForClassifiers(X,y):
+    results = trainClassifiers(X[:500], y[:500])
 
-def main():
-    dataPath = r'C:\Users\Stefan\Documents\Dokumente\Studium\6.Semester\Machine Learning and Pattern Classification\Project\train\1.music.arff'
-    print("Start Program")
+    plt.figure(figsize=(15, 10))
+    plt.title("Mean NSE for all sequence lengths")
+    plt.ylabel("Classification Accuracy")
+    plt.xlabel("Models")
+    plt.boxplot(results.values(), showmeans=True, notch=False)
+    plt.xticks(range(1, len(results.keys()) + 1), results.keys(), rotation='horizontal')
+    plt.show()
+
+def getData(dataPath):
     with open(dataPath, 'r') as f:
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.arff.loadarff.html
         ## Read arff
@@ -16,24 +24,24 @@ def main():
         ## Convert to a datafram for plotting
         dataset = pd.DataFrame(data)
 
-        #Replace with real features we want to use but always include the class column
+        # Replace with real features we want to use but always include the class column
         features = ["f000705", "f000704", "class"]
-        #dataset = dataset[features]
+        # dataset = dataset[features]
         print(dataset.head())
 
-    #Split into data and labels
+    # Split into data and labels
     X = dataset.iloc[:, :-1].values
     y = np.array([1 if str(w, 'utf-8') == 'music' else 0 for w in dataset.iloc[:, -1]])
+    return X, y
 
-    results = trainClassifiers(X[:500],y[:500])
+def hyerParamSearchAll():
+    return 0
 
-    plt.figure(figsize=(15,10))
-    plt.title("Mean NSE for all sequence lengths")
-    plt.ylabel("Classification Accuracy")
-    plt.xlabel("Models")
-    plt.boxplot(results.values(), showmeans=True, notch=False)
-    plt.xticks(range(1, len(results.keys()) + 1), results.keys(), rotation='horizontal')
-    plt.show()
+def main():
+    dataPath = r'C:\Users\Stefan\Documents\Dokumente\Studium\6.Semester\Machine Learning and Pattern Classification\Project\train\1.music.arff'
+    print("Start Program")
+    X, y = getData(dataPath)
+    getAccuarcyForClassifiers(X,y)
 
 if __name__ == '__main__':
     main()
--- Code/train.py
+++ Code/train.py
@@ -1,5 +1,6 @@
 ## Fitting classifier to the Training set
 from sklearn import linear_model
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.dummy import DummyClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
@@ -15,7 +16,17 @@ from sklearn.ensemble import RandomForestClassifier
 def getModelTypes():
     return ['Baseline','Naive', 'QDAnalysis', 'KNN', 'SVM', 'DecisionTree', 'RandomForest', 'NN']
 
-def trainClassifiers(X, y):
+def performGridSearch(train, train_labels, test,estimator, param_grid):
+    RSEED = 0
+    rs = RandomizedSearchCV(estimator, param_grid, random_state=RSEED)  # add code here
+
+    # Fit
+    rs.fit(train, train_labels)
+    rs.best_params_
+    best_model = rs.best_estimator_
+    rf_probs = best_model.predict_proba(test)[:, 1]
+
+def searchParamsForClassifiers(X, y):
     model_types = getModelTypes()
     folds = 5
     all_metrics = {}
@@ -38,8 +49,47 @@ def trainClassifiers(X, y):
             classifier = DecisionTreeClassifier()
         elif model == 'RandomForest':
             classifier = RandomForestClassifier(n_estimators=100)
+            param_grid = {
+                'n_estimators': np.linspace(10, 200).astype(int),
+                'max_depth': [None] + list(np.linspace(3, 20).astype(int)),
+                'max_features': ['auto', 'sqrt', None] + list(np.arange(0.5, 1, 0.1)),
+                'max_leaf_nodes': [None] + list(np.linspace(10, 50, 500).astype(int)),
+                'min_samples_split': [2, 5, 10],
+                'bootstrap': [True, False]
+            }
         elif model == 'NN':
             classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2))
+    performGridSearch(X,y, None, classifier, param_grid)
+
+    return all_metrics
+
+
+def trainClassifiers(X, y):
+    RSEED = 0
+    model_types = getModelTypes()
+    folds = 5
+    all_metrics = {}
+
+    for model in model_types:
+        #Choose Model
+        if model == 'Naive':
+            classifier = GaussianNB()
+        elif model == 'Baseline':
+            classifier = DummyClassifier(strategy = "most_frequent",random_state=RSEED)
+        elif model == 'LinearModel':
+            classifier = linear_model.LinearRegression()
+        elif model == 'QDAnalysis':
+            classifier = QuadraticDiscriminantAnalysis()
+        elif model == 'KNN':
+            classifier = KNeighborsClassifier()
+        elif model == 'SVM':
+            classifier = SVC(kernel='linear',random_state=RSEED)
+        elif model == 'DecisionTree':
+            classifier = DecisionTreeClassifier(random_state=RSEED)
+        elif model == 'RandomForest':
+            classifier = RandomForestClassifier(n_estimators=100,random_state=RSEED)
+        elif model == 'NN':
+            classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2),random_state=RSEED)
 
         #Perform Cross Validation for the current model
         all_metrics[model] = cross_val_score(classifier, X, y, cv=folds, scoring=make_scorer(accuracy_score), error_score = "raise")
