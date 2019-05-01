7a986bb93a0179aa803409ac6ac21bd02b2e31b8
Stefan <moser.stefan1809@gmail.com>
2019-05-01 18:20:39 +0200

    main.py ======= Added multiple File loading

--- Code/main.py
+++ Code/main.py
@@ -22,7 +22,7 @@ def plotAccuarcyForClassifiers(X,y):
     plt.boxplot(accuracies, showmeans=True, notch=False)
     plt.xticks(range(1, len(results.keys()) + 1), results.keys(), rotation='horizontal')
     plt.show()
-    
+
 import pickle
 def pickleDictionaryTo(results_dict, path=None):
     if path is None:
@@ -46,29 +46,32 @@ def saveResultsTo_csv(results_dict):
 from scipy.io import arff    
 def getData(dataPath):
     """Loads matrix of features X and vector of labels y given one .arff file"""
-    with open(dataPath, 'r') as f:
-        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.arff.loadarff.html
-        ## Read arff
-        data, meta = arff.loadarff(f)
-        ## Convert to a dataframe
-        dataset = pd.DataFrame(data)
 
-        # Replace with real features we want to use but always include the class column
-#        features = ["f000705", "f000704", "class"]
-#        dataset = dataset[features]
-#        print(dataset.head())
+    fileName = "{}/{}.music.arff"
+    dataset = None
+    for i in range(1,4):
+        with open(fileName.format(dataPath,i), 'r') as f:
+            # https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.arff.loadarff.html
+            ## Read arff
+            data, meta = arff.loadarff(f)
+            ## Convert to a dataframe
+            print(fileName.format(dataPath,i))
+            if i == 1:
+                dataset = pd.DataFrame(data)
+            else:
+                dataset = pd.concat([dataset, pd.DataFrame(data)], ignore_index=True)
 
     # Split into data and labels
     X = dataset.iloc[:, :-1].values
     y = np.array([1 if str(w, 'utf-8') == 'music' else 0 for w in dataset.iloc[:, -1]])
     return X, y
 
-
 def main():
-    dataPath = '../data/train_arff/1.music.arff'
+    dataPath = '../data/train_arff'
     print("Start Program")
     X, y = getData(dataPath)
-    plotAccuarcyForClassifiers(X, y)
+    #plotAccuarcyForClassifiers(X, y)
+
 
 if __name__ == '__main__':
     main()
