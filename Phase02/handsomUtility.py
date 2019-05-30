300708ef139aa7f0a59186e40180f821b658923e
Kareem Jeiroudi <kareemjeiroudi@gmail.com>
2019-05-30 16:51:38 +0200

    Finished best classifier analysis

--- Phase02/handsomUtility.py
+++ Phase02/handsomUtility.py
@@ -5,16 +5,17 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_curve, auc, roc_auc_score, accuracy_score, confusion_matrix
 
 
-def getCost(y_true, y_pred, **kwargs):
+def profit_score(y_true, y_pred, **kwargs):
     rewardDict = dict()
     #(True Value, Predicted Value)
-    rewardDict[(0,0)] = 0
-    rewardDict[(0,1)] = -3
-    rewardDict[(1,0)] = 0
-    rewardDict[(1,1)] = 1
+    rewardDict[(0,0)] = 1
+    rewardDict[(0,1)] = 0.20
+    rewardDict[(1,0)] = -2
+    rewardDict[(1,1)] = 0.20
     
     reward = 0
     if len(y_true) != len(y_pred):
@@ -36,29 +37,31 @@ def plotROC(solution, prediction, classifierName):
     plt.ylim([-0.05, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('ROC for {}'.format(classifierName))
+    plt.title('ROC for {}'.format(classifierName), fontsize=20)
     plt.legend()
     plt.show()
+
+    return roc_auc
     
 def getClassifiers():
     logistic_regression = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                                         intercept_scaling=1, max_iter=100, multi_class='warn',
-                                         n_jobs=None, penalty='l2', random_state=None, solver='liblinear',
-                                         tol=0.0001, verbose=0, warm_start=False)
+                                             intercept_scaling=1, max_iter=100, multi_class='warn',
+                                             penalty='l2', random_state=None, solver='liblinear',
+                                             tol=0.0001, verbose=0, warm_start=False)
     random_forest = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='entropy',
-                                       max_depth=1000, max_features='auto', max_leaf_nodes=None,
-                                       min_impurity_decrease=0.0, min_impurity_split=None,
-                                       min_samples_leaf=2, min_samples_split=6,
-                                       min_weight_fraction_leaf=0.0, n_estimators=50, n_jobs=None,
-                                       oob_score=False, random_state=0, verbose=0, warm_start=False)
+                                           max_depth=1000, max_features='auto', max_leaf_nodes=None,
+                                           min_impurity_decrease=0.0, min_impurity_split=None,
+                                           min_samples_leaf=2, min_samples_split=6,
+                                           min_weight_fraction_leaf=0.0, n_estimators=50, n_jobs=-1,
+                                           oob_score=False, random_state=0, verbose=0, warm_start=False)
     nn = MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
-                   beta_2=0.999, early_stopping=False, epsilon=1e-08,
-                   hidden_layer_sizes=100, learning_rate='constant',
-                   learning_rate_init=0.001, max_iter=50, momentum=0.9,
-                   n_iter_no_change=10, nesterovs_momentum=True, power_t=0.5,
-                   random_state=0, shuffle=True, solver='adam', tol=0.0001,
-                   validation_fraction=0.1, verbose=False, warm_start=False)
-    voting = VotingClassifier(estimators=[('lr', logistic_regression), ('rf', random_forest), ('nn', nn)],voting='soft')
+                       beta_2=0.999, early_stopping=False, epsilon=1e-08,
+                       hidden_layer_sizes=100, learning_rate='constant',
+                       learning_rate_init=0.001, momentum=0.9,
+                       n_iter_no_change=10, nesterovs_momentum=True, power_t=0.5,
+                       random_state=0, shuffle=True, solver='sgd', tol=0.0001,
+                       validation_fraction=0.1, verbose=False, warm_start=False)
+    voting = VotingClassifier(estimators=[('lr', logistic_regression), ('rf', random_forest), ('nn', nn)], voting="soft")
     return [(logistic_regression,"logistic_regression"), (random_forest,"random_forest"), (nn,"Neural Network"), (voting, "ensemble")]
 
 
@@ -122,3 +125,139 @@ def gain_matrix(y_true, y_pred, **kwargs):
     for index in range(len(y_true)):
         reward += rewardDict[(y_true[index], y_pred[index])]
     return reward
+
+
+def loadPCA():
+    X_train_pca = pd.read_csv(r'../Data/speech_preprocessed/speech_train_pca.csv')
+    # undersampling
+    y_train_pca = np.array(X_train_pca.iloc[:, -1])
+    non_music = X_train_pca[y_train_pca == 0]
+    music = X_train_pca[y_train_pca == 1][:len(X_train_pca[y_train_pca == 0])]
+    X_train_pca = music.append(non_music, ignore_index=True).sample(frac=1, random_state = 0)
+    y_train_pca = np.array(X_train_pca.iloc[:, -1])
+    X_train_pca = X_train_pca.iloc[:,:-1].values
+    print("Loaded PCA training set")
+
+    X_val_pca = pd.read_csv(r'../Data/speech_preprocessed/speech_validation_pca.csv').values
+    y_val_pca = X_val_pca[:,-1]
+    X_val_pca = X_val_pca[:,:-1]
+    print("Loaded PCA validation set")
+    
+    X_test_pca = pd.read_csv(r'../Data/speech_preprocessed/speech_test_pca.csv').values
+    y_test_pca = X_test_pca[:,-1]
+    X_test_pca = X_test_pca[:,:-1]
+    print("Loaded PCA test set")
+    
+    scaler = StandardScaler()
+    scaler.fit(X_train_pca)
+    X_train_scaled = scaler.transform(X_train_pca)
+    X_val_scaled = scaler.transform(X_val_pca)
+    X_test_scaled = scaler.transform(X_test_pca)
+    
+    return (X_train_scaled, y_train_pca, X_val_scaled, y_val_pca, X_test_scaled, y_test_pca)
+
+
+# FIXME: change the best-scoring model
+def parameterGridSearch():
+    parameter_space = {
+        'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+        'activation': ['tanh', 'relu', 'logistic'],
+        'solver': ['sgd', 'adam', 'lbfgs'],
+        'alpha': [0.0001, 0.05],
+        'learning_rate': ['constant','adaptive']}
+
+    classifier = MLPClassifier(max_iter=50)
+    name = "Neural Network"
+
+    clf = GridSearchCV(classifier, parameter_space, n_jobs=-1, cv=3, scoring=make_scorer(profit_score), iid = False)
+    clf.fit(X_train, y_train)
+
+    # Best paramete set
+    print('Best parameters found:\n', clf.best_params_)
+
+    # All results
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+    y_true, y_pred = y_val , clf.predict(X_val)
+
+    print('Results on the test set:')
+    print(classification_report(y_true, y_pred))
+
+def analyzePCA():
+    max_val_score = np.sum(y_val_pca == 1)
+    print("===============================")
+    print("|Training on PCA")
+    print("===============================")
+
+    for classifier, name in getClassifiers():
+        print(name)
+        classifier.fit(X_train_pca, y_train_pca)
+        y_pred = post_processing(classifier.predict(X_val_pca))
+        accuracy = np.around(accuracy_score(y_val_pca, y_pred) * 100, decimals = 2)
+        val_profit = profit_score(y_val_pca, y_pred)
+        print("|-Confusion Matrix: {}".format(confusion_matrix(y_val_pca, y_pred).ravel()/len(y_pred)))
+        print("|-Accuracy: {}".format(accuracy))
+        print("|-Profit: {} / {}%".format(val_profit, np.around(val_profit* 100/max_val_score,decimals = 2)))
+        print("L==============================")
+        plotROC(y_val_pca, y_pred, name)
+
+def findBestClassifier():
+    max_val_score = np.sum(y_val == 1)
+    accuracies = []
+    profits = []
+    print("Max possible Profit of the validation set: {}".format(max_val_score))
+
+    for column in features:
+        f = features[column].dropna().tolist()
+        print("===============================")
+        print("|Training on {}".format(column))
+        print("===============================")
+        X_train = dataset_train[f].values
+        X_val = dataset_val[f].values
+            #Scaling all features to mean 0 variance 1
+        scaler = StandardScaler()
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_val = scaler.transform(X_val)
+
+        for classifier, name in getClassifiers():
+            print(name)
+            classifier.fit(X_train, y_train)
+            y_pred = post_processing(classifier.predict(X_val))
+            accuracy = np.around(accuracy_score(y_val, y_pred) * 100, decimals = 2)
+            val_profit = profit_score(y_val, y_pred)
+            accuracies.append(accuracy)
+            profits.append(val_profit)
+            print("|-Confusion Matrix: {}".format(confusion_matrix(y_val, y_pred).ravel()/len(y_pred)))
+            print("|-Accuracy: {}%".format(accuracy))
+            print("|-Profit: {} / {}%".format(val_profit, np.around(val_profit* 100/max_val_score,decimals = 2)))
+            print("L==============================")
+            plotROC(y_val, y_pred, name)
+    print(max(accuracies))
+    print(max(profits))
+
+def trainBestClassifier():
+    f = features.SVM.dropna().tolist()
+    X_train = dataset_train[f].values
+    X_val = dataset_val[f].values
+
+    #Scaling all features to mean 0 variance 1
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
+
+    for classifier, name in getClassifiers():    
+        classifier.fit(X_train_scaled, y_train)
+
+        y_pred = post_processing(classifier.predict(X_val_scaled))
+        accuracy = np.around(accuracy_score(y_val, y_pred) * 100, decimals=2)
+        val_profit = profit_score(y_val, y_pred)
+        print("Confusion Matrix: {}".format(confusion_matrix(y_val, y_pred).ravel()/len(y_pred)))
+        print("Accuracy: {}%".format(accuracy))
+        print("Profit: {} / {}%".format(val_profit, np.around(val_profit* 100/max_val_score, decimals=2)))
+        plotROC(y_val, y_pred, name)
+
